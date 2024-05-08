a35992f4d41e183a6ff2d8502e7620391dc93eca
S4vi0r <gbp17@outlook.com>
2024-05-08 14:41:56 -0500

    chore: Refactor matrix initialization and traversal

--- repaso parcial/arreglos/matrix.go
+++ repaso parcial/arreglos/matrix.go
@@ -6,7 +6,8 @@ import (
 
 func main() {
 
-	var matrix [3][3]int = [3][3]int{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}
+	// var matrix [3][3]int = [3][3]int{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}
+	matrix := [3][3]int{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}
 
 	// for i := 0; i < len(matrix); i++ {
 	// 	for j := 0; j < len(matrix[i]); j++ {
