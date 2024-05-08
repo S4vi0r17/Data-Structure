0ae9bbf134572f38250e4bc431a796be208dc583
S4vi0r <gbp17@outlook.com>
2024-05-08 14:11:53 -0500

    chore: Comment out matrix input code and add column traversal

--- repaso parcial/arreglos.go
+++ repaso parcial/arreglos.go
@@ -6,14 +6,14 @@ import (
 
 func main() {
 
-	var matrix [3][3]int
+	var matrix [3][3]int = [3][3]int{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}
 
-	for i := 0; i < len(matrix); i++ {
-		for j := 0; j < len(matrix[i]); j++ {
-			fmt.Print("Ingrese un valor: [", i, "][", j, "]: ")
-			fmt.Scanf("%d\n", &matrix[i][j])
-		}
-	}
+	// for i := 0; i < len(matrix); i++ {
+	// 	for j := 0; j < len(matrix[i]); j++ {
+	// 		fmt.Print("Ingrese un valor: [", i, "][", j, "]: ")
+	// 		fmt.Scanf("%d\n", &matrix[i][j])
+	// 	}
+	// }
 
 	fmt.Println("Recorrido por filas")
 	for i := 0; i < len(matrix); i++ {
@@ -27,6 +27,14 @@ func main() {
 	for i := 0; i < len(matrix); i++ {
 		for j := 0; j < len(matrix[i]); j++ {
 			fmt.Print(matrix[j][i], " ")
+		}
+		fmt.Println()
+	}
+
+	fmt.Println("Recorrido por columnas 2")
+	for j := 0; j < len(matrix); j++ {
+		for i := 0; i < len(matrix[i]); i++ {
+			fmt.Print(matrix[i][j], " ")
 		}
 		fmt.Println()
 	}
