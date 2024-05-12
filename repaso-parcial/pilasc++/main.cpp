1d701a6b2462903b20e00e489f7730d69b7d201e
S4vi0r <gbp17@outlook.com>
2024-05-11 22:28:31 -0500

    Refactor stack pop function and update main function

--- repaso-parcial/pilasc++/main.cpp
+++ repaso-parcial/pilasc++/main.cpp
@@ -18,6 +18,16 @@ void push(Node *&stack, int data)
     cout << "Value: " << data << endl;
 }
 
+void pop(Node *&stack, int &data)
+{
+    Node *aux = stack;
+    data = aux->value;
+    stack = aux->next;
+    delete aux;
+
+    cout << "Value deleted: " << data << endl;
+}
+
 main()
 {
     Node *stack = nullptr;
@@ -25,4 +35,6 @@ main()
 
     push(stack, n1);
     push(stack, n2);
+    pop(stack, n2);
+    pop(stack, n1);
 }
